2a4af2233b7fd1110b7ab823edc49a2610550231
Steven-sdx <steven.reed@smarterdx.com>
2025-01-06 09:54:35 -0600

    Rob-b interview branch

--- src/pages/api/codes.ts
+++ src/pages/api/codes.ts
@@ -1,13 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-type Data = {
-  name: string;
+type CodeT = {
+  code: string;
+  short_description: string;
+  long_description: string;
 };
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Array<CodeT>>
 ) {
-  res.status(200).json({ name: 'John Doe' });
+  res.status(200).json([]);
 }
